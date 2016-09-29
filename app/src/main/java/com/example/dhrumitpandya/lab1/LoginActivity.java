3bef191140f16adcbfce4c6aace97f23242b5fe4
Dhrumit Pandya <pand0039@algonquinlive.com>
2016-09-29 14:53:58 -0400

    Initial commit

--- app/src/main/java/com/example/dhrumitpandya/lab1/LoginActivity.java
+++ app/src/main/java/com/example/dhrumitpandya/lab1/LoginActivity.java
@@ -1,17 +1,26 @@
 package com.example.dhrumitpandya.lab1;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.i(ACTIVITY_NAME, "In onCreate()");
         setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -25,5 +34,56 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+        @Override
+        protected void onResume(){
+            super.onResume();
 
-}
+            Log.i(ACTIVITY_NAME,"In onResume()");
+        }
+
+
+        protected  void onStart(){
+            super.onStart();
+
+            Log.i(ACTIVITY_NAME, "In onStart()");
+            final SharedPreferences prefs = getSharedPreferences("Default", Context.MODE_PRIVATE);
+            prefs.getString("DefaultEmail","dhrumitpandya26@gmail.com");
+            Button login_button = (Button) findViewById(R.id.button2);
+            login_button.setOnClickListener(new View.OnClickListener() {
+                                                @Override
+                                                public void onClick(View v) {
+
+                                                    EditText name = (EditText)findViewById(R.id.textView2);
+                                                    SharedPreferences.Editor editor = prefs.edit();
+                                                    String x =  name.getText().toString() ;
+                                                    editor.putString("loginname" ,x );
+                                                    editor.commit();
+                                                    Intent intent = new Intent(LoginActivity.this, StartActivity.class);
+                                                    startActivity(intent);
+                                                }
+                                            }
+            );
+
+        }
+
+
+        protected  void onPause(){
+            super.onPause();
+
+            Log.i(ACTIVITY_NAME, "In onPause()");
+        }
+
+
+        protected  void onStop(){
+            super.onStop();
+
+            Log.i(ACTIVITY_NAME, "In onStop()");
+        }
+
+
+        protected  void onDestroy(){
+            super.onDestroy();
+
+            Log.i(ACTIVITY_NAME, "In onDestroy()");
+        }
+    }
--- app/src/main/java/com/example/dhrumitpandya/lab1/StartActivity.java
+++ app/src/main/java/com/example/dhrumitpandya/lab1/StartActivity.java
@@ -2,12 +2,51 @@ package com.example.dhrumitpandya.lab1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends AppCompatActivity {
+    protected static final String ACTIVITY_NAME = "StartActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.i(ACTIVITY_NAME, "On Create .....");
         setContentView(R.layout.activity_start);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "On Resume .....");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "On Destroy .....");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "On Pause .....");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(ACTIVITY_NAME, "On Restart .....");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "On Start .....");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "On Stop .....");
     }
 }
